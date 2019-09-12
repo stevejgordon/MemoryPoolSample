d14fdd80a15abcd55c3826d506d78c9d05a5e41e
Steve Gordon <sgordon@hotmail.co.uk>
2019-09-12 13:16:18 +0100

    Demo ownership

--- MemoryPoolSample/Program.cs
+++ MemoryPoolSample/Program.cs
@@ -9,17 +9,28 @@ namespace MemoryPoolSample
         {
             var pool = MemoryPool<byte>.Shared;
 
-            using IMemoryOwner<byte> rental = pool.Rent(minBufferSize: 1024);
+            using IMemoryOwner<byte> owner = pool.Rent(minBufferSize: 1024);
 
-            DoSomethingWithMemory(rental.Memory);
+            DoSomethingWithMemory(owner.Memory); // consumes memory
 
             // The memory is released back to the pool when the IMemoryOwner is
             // disposed after this method exits.
+
+            var secondOwner = pool.Rent(minBufferSize: 1024);
+
+            TakeOwnershipOfMemory(secondOwner); // transfer ownership
         }
 
         private static void DoSomethingWithMemory(Memory<byte> someBytes)
         {
             // do some stuff!
+        }
+
+        private static void TakeOwnershipOfMemory(IMemoryOwner<byte> owner)
+        {
+            DoSomethingWithMemory(owner.Memory); // consume memory
+
+            owner.Dispose(); // release memory
         }
     }
 }
