224ef926607d82a175bfca029e63a396e94bc661
Steve Gordon <sgordon@hotmail.co.uk>
2019-09-12 13:08:32 +0100

    Initial

---
+++ MemoryPoolSample/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Buffers;
+
+namespace MemoryPoolSample
+{
+    class Program
+    {
+        static void Main()
+        {
+            var pool = MemoryPool<byte>.Shared;
+
+            using IMemoryOwner<byte> rental = pool.Rent(minBufferSize: 1024);
+
+            DoSomethingWithMemory(rental.Memory);
+
+            // The memory is released back to the pool when the IMemoryOwner is
+            // disposed after this method exits.
+        }
+
+        private static void DoSomethingWithMemory(Memory<byte> someBytes)
+        {
+            // do some stuff!
+        }
+    }
+}
